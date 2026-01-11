7ef01524aa8b6d7c2fb8c898cb37e72aae020343
Ellis Breen <1263585+griels@users.noreply.github.com>
2026-01-11 11:43:09 +0000

    Update Synergy version to 3.5.1

--- Casks/synergy3.rb
+++ Casks/synergy3.rb
@@ -1,7 +1,7 @@
 cask "synergy3" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.3.1"
+  version "3.5.1"
   sha256 arm:   "1aa7e92182623d4bba7da48c98c27e67f5aaa15bb8481c6c963fb6990ba77eae",
          intel: "d4a36ad0c10c0d6f74a3ea2a9d88fe77ac9f551ff06de8654d3ecfeb93d49ac4"
 
